ef91cdec9913983309a795fce93079113252eff9
Mehdi Kord <mehdi.kord171@gmail.com>
2025-06-08 15:32:37 +0330

    update

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path : '/landing/forms/:token',
+      path : '/:token',
       component : Forms
     }
   ],
